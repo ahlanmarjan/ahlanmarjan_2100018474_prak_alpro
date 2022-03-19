b12ee938c98251c119a92e34b7a11d1c82be9c41
MuhammadRidhoAqilMunif <101913333+MuhammadRidhoAqilMunif@users.noreply.github.com>
2022-03-19 23:24:47 +0700

    Update program kalkulator sederhana.cpp

--- program kalkulator sederhana.cpp
+++ program kalkulator sederhana.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tunio.h>
 using namespace std;
  
 int main() {
@@ -69,5 +70,6 @@ int main() {
     else{
     cout << "terima kasih anda telah keluar";
     }
+    getch();
     return 0;
 }
